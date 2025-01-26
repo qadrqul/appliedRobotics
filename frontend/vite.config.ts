74414f9904a583439dd4cb97d383338b54594d49
qadrqul <jagpanthertitanium@gmail.com>
2025-01-26 19:51:39 +0100

    Fix issues

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,8 +6,12 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: '0.0.0.0',
     port: 8000,
+    hmr: {
+      host: '192.168.178.149',
+      protocol: 'ws',
+    },
   },
   plugins: [
     react(),
